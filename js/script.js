359b20811ddb98302ce4936a2856d17f78d152c1
TatiynaLN <68844852+TatiynaLN@users.noreply.github.com>
2020-11-21 21:30:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,97 @@
+//ANIMASION
+// $(function () {
+
+// });
+const animItems = document.querySelectorAll('.anim-items, .anim');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	// function animOnScroll(params)
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				// убираем анимацию при прокрукте в верх
+				if (!animItem.classList.contains('anim__non')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+	animOnScroll();
+}
+//FORMA
+$(document).ready(function () {
+
+	//E-mail Ajax Send
+	$("form").submit(function () { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function () {
+			alert("Thank you!");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+
+//SLIDER
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 1,
+		// autoplay: true,
+		autoplay: false,
+		speed: 2000,
+		autoplaySpeed: 800,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+});
+
+
+
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
