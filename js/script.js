020daa7960a3639c1e17745c0e23125f4fdb8f8e
TatiynaLN <68844852+TatiynaLN@users.noreply.github.com>
2020-11-21 21:28:19 +0300

    preloader

--- js/script.js
+++ js/script.js
@@ -86,3 +86,13 @@ $(document).ready(function () {
 	});
 });
 
+
+//preloader
+
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
